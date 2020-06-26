302300f634864e681af563c6bf1d492d4e9979d7
Furion <artemfurletov6@gmail.com>
2020-06-26 16:53:31 +0300

    meal data for prefab

---
+++ Assets/MealData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct MealDataStats
+{
+    public int satiety;
+
+    public MealDataStats(int satiety)
+    {
+        this.satiety = satiety;
+    }
+}
+public class MealData : MonoBehaviour
+{
+    public MealDataStats mealStats;
+
+}
