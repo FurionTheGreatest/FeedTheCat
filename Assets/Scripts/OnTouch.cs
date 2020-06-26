b83fa083cca348ed8e5f99ceab3eb247993e8c24
Furion <artemfurletov6@gmail.com>
2020-06-26 16:51:46 +0300

    deploy satiety throug event, disable collider on tap, alpha changing improved

--- Assets/Scripts/OnTouch.cs
+++ Assets/Scripts/OnTouch.cs
@@ -5,9 +5,10 @@ using System.Numerics;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
+
 public class OnTouch : MonoBehaviour
 {
-    public delegate void OnMealTouched();
+    public delegate void OnMealTouched(int satiety);
     public static event OnMealTouched UpdateUi;
 
     private Transform _catMouth;
@@ -17,6 +18,11 @@ public class OnTouch : MonoBehaviour
 
     private Rigidbody2D _rb;
     private SpriteRenderer _sprite;
+    private CircleCollider2D _collider2D;
+
+    public int satiety;
+    public bool destroyFood = false;
+    private const float SpeedOfFadeOut = 3f;
 
     private void Start()
     {
@@ -24,17 +30,19 @@ public class OnTouch : MonoBehaviour
         _step = Speed * Time.deltaTime;
         _rb = gameObject.GetComponent<Rigidbody2D>();
         _sprite = GetComponent<SpriteRenderer>();
+        _collider2D = GetComponent<CircleCollider2D>();
+        satiety = GetComponent<MealData>().mealStats.satiety;
     }
 
-    private void OnMouseDown()
+    public void OnMouseDown()
     {
         _isClicked = true;
-        ChangeMealAlpha();
-        UpdateUi?.Invoke();
+        StartCoroutine(ChangeMealAlpha(0.3f));
+        UpdateUi?.Invoke(satiety);
     }
-
     private void TranslateMealToMouth()
     {
+        _collider2D.enabled = false;
         _rb.bodyType = RigidbodyType2D.Static;
         transform.position = Vector3.MoveTowards(transform.position, _catMouth.position, _step);
         
@@ -45,14 +53,23 @@ public class OnTouch : MonoBehaviour
 
     private void Update()
     {
+        if (destroyFood && !_isClicked)
+            StartCoroutine(ChangeMealAlpha(0f));
+        
         if(!_isClicked) return;
         TranslateMealToMouth();
     }
 
-    private void ChangeMealAlpha()
+    private IEnumerator ChangeMealAlpha(float alphaOfMeal)
     {
         var alpha = _sprite.color;
-        alpha.a = 0.3f;
-        _sprite.color = alpha;
+        while (alpha.a >= alphaOfMeal)
+        {
+            alpha.a -= Time.deltaTime * SpeedOfFadeOut;
+            _sprite.color = alpha;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        if(alpha.a <= 0)
+            Destroy(gameObject);
     }
 }
