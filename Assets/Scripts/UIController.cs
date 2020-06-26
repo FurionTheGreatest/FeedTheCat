1fae7d7aac88277ffe8ff4b9c4d9706baa21a51b
Furion <artemfurletov6@gmail.com>
2020-06-26 16:50:09 +0300

    slider shows how much food catched and how much needed to be catched

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,22 +8,44 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public TMP_Text uICounter;
+    public Slider satietySlider;
+    public Slider foodLeftSlider;
+
+    public int maxSatietyValue;
     private const string CounterText = "Food counter: ";
     private int _score = 0;
-    
+    private int _currentSatiety;
+    private FoodSupplyManager _foodSupplyManager;
+    private void Awake()
+    {
+        _foodSupplyManager = FindObjectOfType<FoodSupplyManager>();
+        maxSatietyValue = _foodSupplyManager.desiredSatiety;
+        
+        satietySlider.maxValue = maxSatietyValue;
+        satietySlider.value = 0;
+        foodLeftSlider.maxValue = maxSatietyValue + (maxSatietyValue/2);
+        foodLeftSlider.value = foodLeftSlider.maxValue;
+    }
+
     private void UpdateCounter()
     {
         _score ++;
         uICounter.text = CounterText + _score;
     }
+
+    private void UpdateSatietySliderValue(int satiety)
+    {
+        satietySlider.value += satiety;
+        foodLeftSlider.value -= satiety;
+    }
     
     private void OnEnable()
     {
-        OnTouch.UpdateUi += UpdateCounter;
+        OnTouch.UpdateUi += UpdateSatietySliderValue;
     }
     
     private void OnDisable()
     {
-        OnTouch.UpdateUi -= UpdateCounter;
+        OnTouch.UpdateUi -= UpdateSatietySliderValue;
     }
 }
