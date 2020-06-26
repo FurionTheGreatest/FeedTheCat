5b6c9bd154a88eed61247202c45bb3c978e3de46
Furion <artemfurletov6@gmail.com>
2020-06-26 16:50:42 +0300

    require component camera and minor variable naming improvement

--- Assets/Scripts/TouchHandler.cs
+++ Assets/Scripts/TouchHandler.cs
@@ -2,49 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class TouchHandler : MonoBehaviour
 {
-    [SerializeField] private Camera _camera;
-    private Vector3 touchPosWorld;
+    public Camera sceneCamera;
+    private Vector3 _touchPosWorld;
 
-    TouchPhase touchPhase = TouchPhase.Began;
+    private const TouchPhase BeganTouchPhase = TouchPhase.Began;
 
     private void Update()
     {
 #if UNITY_ANDROID
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == BeganTouchPhase)
         {
-            Debug.Log("Touch");
+            //Debug.Log("Touch");
 
             //We transform the touch position into word space from screen space and store it.
-            touchPosWorld = _camera.ScreenToWorldPoint(Input.GetTouch(0).position);
+            _touchPosWorld = sceneCamera.ScreenToWorldPoint(Input.GetTouch(0).position);
 
-            Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+            Vector2 touchPosWorld2D = new Vector2(_touchPosWorld.x, _touchPosWorld.y);
 
             //We now raycast with this information. If we have hit something we can process it.
             RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, transform.forward);
 
             if (hitInformation.collider == null) return;
             GameObject touchedObject = hitInformation.transform.gameObject;
-            Debug.Log("Touched " + touchedObject.transform.name);
         }
 #endif
 #if UNITY_EDITOR || UNITY_STANDALONE_WIN
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log("ClickMouse");
+            //Debug.Log("ClickMouse");
 
             //We transform the touch position into word space from screen space and store it.
-            touchPosWorld = _camera.ScreenToWorldPoint(Input.mousePosition);
+            _touchPosWorld = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
 
-            Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+            Vector2 touchPosWorld2D = new Vector2(_touchPosWorld.x, _touchPosWorld.y);
 
             //We now raycast with this information. If we have hit something we can process it.
             RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, transform.forward);
 
             if (hitInformation.collider == null) return;
             GameObject touchedObject = hitInformation.transform.gameObject;
-            Debug.Log("Touched " + touchedObject.transform.name);
+            //Debug.Log("Touched " + touchedObject.transform.name);
         } 
 #endif
     }
